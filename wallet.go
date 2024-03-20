11730b1289cf71aed5f68a448d393799d144bc1f
s-Arunagiri-s <ssarunagiri2002@gmail.com>
2024-03-20 09:36:44 +0530

    4th requirment done

--- wallet.go
+++ wallet.go
@@ -1,8 +1,139 @@
 package wallet
-var ConRate=82.47
 
-func CheckInrToUsdConRate( UserGuessConRate float64) bool{
-    if(UserGuessConRate == ConRate){
+import (
+	"errors"
+	"math"
+)
+
+// Money stored in INR regardless of Input Currency.
+type Wallet struct {
+	balanceInr *MoneyObject
+}
+
+/*
+	type Currency struct{
+		Name string
+		conversionRateToInr float64
+	}
+*/
+type MoneyObject struct {
+	amount   float64
+	currency string
+}
+
+var exchangeRates = map[string]float64{}
+
+func init() {
+	AddCurrencyAndConvRateToInr("USD", 82.47)
+	AddCurrencyAndConvRateToInr("INR", 1)
+	AddCurrencyAndConvRateToInr("SGD", 61.87)
+}
+
+func (mb *MoneyObject) roundFloatInr() {
+	val := mb.amount
+	ratio := math.Pow(10, float64(2))
+	mb.amount = math.Round(val*ratio) / ratio
+}
+
+func AddCurrencyAndConvRateToInr(currency string, convRate float64) {
+	exchangeRates[currency] = convRate
+}
+
+func NewMoneyObject(amount float64, currency string) (*MoneyObject, error) {
+
+	_, errConv := GetConRateFromInr(currency)
+	if amount < 0 {
+		return nil, errors.New("invalid amount for money object")
+	} else if errConv != nil {
+		return nil, errors.New("currency not supported")
+	}
+	return &MoneyObject{
+		amount:   amount,
+		currency: currency,
+	}, nil
+}
+
+func (mb *MoneyObject) GetAmountOfMoneyObject() float64 {
+	return mb.amount
+}
+
+func (mb *MoneyObject) GetCurrencyOfMoneyObject() string {
+	return mb.currency
+}
+
+// create wallet
+func NewWalletBalanceInInr(moneyObject *MoneyObject) *Wallet {
+	return &Wallet{
+		balanceInr: moneyObject,
+	}
+}
+
+// Get conversion rate to INR
+func GetConRateFromInr(currency string) (float64, error) {
+
+	if _, ok := exchangeRates[currency]; ok {
+		return float64(exchangeRates[currency]), nil
+	} else {
+		return -1, errors.New("currency not supported")
+	}
+}
+
+// Converter
+func ConverterToInr(moneyObject MoneyObject) (*MoneyObject, error) {
+	ConRate, _ := GetConRateFromInr(moneyObject.currency)
+
+	moneyObjectRet, errMoneyObject := NewMoneyObject(moneyObject.amount*ConRate, "INR")
+	if errMoneyObject != nil {
+		return nil, errMoneyObject
+	}
+
+	return moneyObjectRet, nil
+}
+
+// return balance
+func (w *Wallet) GetMoneyObjectInInr() *MoneyObject {
+	return w.balanceInr
+}
+
+// Add money into wallet
+func (w *Wallet) AddMoney(moneyObject MoneyObject) error {
+	MoneyObjectAddedInInr, _ := ConverterToInr(moneyObject)
+
+	oldBalance := w.GetMoneyObjectInInr()
+	newBalanceInr := oldBalance.amount + MoneyObjectAddedInInr.amount
+	newBalanceInrObj, errMoneyObject := NewMoneyObject(newBalanceInr, "INR")
+	if errMoneyObject != nil {
+		return errMoneyObject
+	}
+	newBalanceInrObj.roundFloatInr()
+	w.balanceInr = newBalanceInrObj
+	return nil
+
+}
+
+// Remove money from the wallet
+func (w *Wallet) DebitMoney(moneyObject MoneyObject) error {
+	MoneyObjectAddedInInr, _ := ConverterToInr(moneyObject)
+
+	oldBalance := w.GetMoneyObjectInInr()
+	newBalanceInr := oldBalance.amount - MoneyObjectAddedInInr.amount
+	if newBalanceInr < 0 {
+		return errors.New("insufficient funds")
+	}
+
+	newBalanceInrObj, errMoneyObject := NewMoneyObject(newBalanceInr, "INR")
+	if errMoneyObject != nil {
+		return errMoneyObject
+	}
+	newBalanceInrObj.roundFloatInr()
+	w.balanceInr = newBalanceInrObj
+	return nil
+}
+
+// Comparision of USD and INR
+func CheckInrToUsdConRate(UserGuessConRate float64) bool {
+	conRate, _ := GetConRateFromInr("USD")
+	if UserGuessConRate == conRate {
 		return true
 	} else {
 		return false
--- wallet_test.go
+++ wallet_test.go
@@ -5,18 +5,260 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
-func TestIf1DollarIs82Rs(t *testing.T){
-	want:= false
-	
-	got:= CheckInrToUsdConRate(82)
 
-    assert.Equal(t,want,got)
+// req 1
+func TestIf1DollarIs82Rs(t *testing.T) {
+	want := false
+
+	got := CheckInrToUsdConRate(82)
+
+	assert.Equal(t, want, got)
 }
 
-func TestIf1DollarIs82point47Rs(t *testing.T){
-	want:= true
-	
-	got:= CheckInrToUsdConRate(82.47)
+func TestIf1DollarIs82point47Rs(t *testing.T) {
+	want := true
 
-    assert.Equal(t,want,got)
+	got := CheckInrToUsdConRate(82.47)
+
+	assert.Equal(t, want, got)
+}
+
+// Money stored in INR regardless of Input Currency.
+// new wallet
+func TestNewWalletWithValidBalance(t *testing.T) {
+	want := 100.0
+	moneyObject, _ := NewMoneyObject(100, "INR")
+
+	wallet := NewWalletBalanceInInr(moneyObject)
+	got := wallet.GetMoneyObjectInInr()
+
+	assert.NotNil(t, wallet)
+	assert.Equal(t, want, got.amount)
+
+}
+
+func TestNewWalletWith0Balance(t *testing.T) {
+	want := 0.0
+	moneyObject, _ := NewMoneyObject(0, "INR")
+
+	wallet := NewWalletBalanceInInr(moneyObject)
+	got := wallet.GetMoneyObjectInInr()
+	assert.NotNil(t, wallet)
+	assert.Equal(t, want, got.amount)
+
+}
+
+// New Money Object
+func TestNewMoneyObjectWithValidAmountAndCurrency(t *testing.T) {
+	amount := 100.0
+	currency := "INR"
+
+	moneyobject, err := NewMoneyObject(amount, currency)
+
+	assert.NotNil(t, moneyobject)
+	assert.Nil(t, err)
+	assert.Equal(t, amount, moneyobject.GetAmountOfMoneyObject())
+	assert.Equal(t, currency, moneyobject.GetCurrencyOfMoneyObject())
+
+}
+
+func TestNewMoneyObjectWith0Balance(t *testing.T) {
+	amount := 0.0
+	currency := "INR"
+
+	moneyobject, err := NewMoneyObject(amount, currency)
+
+	assert.NotNil(t, moneyobject)
+	assert.Nil(t, err)
+	assert.Equal(t, amount, moneyobject.GetAmountOfMoneyObject())
+	assert.Equal(t, currency, moneyobject.GetCurrencyOfMoneyObject())
+
+}
+
+func TestMoneyObjectWithInValidBalance(t *testing.T) {
+	amount := -10.0
+	currency := "INR"
+	moneyobject, err := NewMoneyObject(amount, currency)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, moneyobject)
+	assert.EqualError(t, err, "invalid amount for money object")
+}
+
+func TestMoneyObjectWithUnsupportedCurrency(t *testing.T) {
+	amount := 10.0
+	currency := "EURO"
+	moneyobject, err := NewMoneyObject(amount, currency)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, moneyobject)
+	assert.EqualError(t, err, "currency not supported")
+}
+
+// Converter
+func TestConUsdtoInr(t *testing.T) {
+	inputUsd := 100.0
+	currency := "USD"
+	moneyobject, _ := NewMoneyObject(inputUsd, currency)
+	want := 8247.0
+
+	got, err := ConverterToInr(*moneyobject)
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, got.amount)
+}
+
+// GetConRateFromInr
+func TestGetConRateForUSD(t *testing.T) {
+	want := 82.47
+
+	got, err := GetConRateFromInr("USD")
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, got)
+}
+
+func TestGetConRateForYEN(t *testing.T) {
+	want := -1.0
+
+	got, err := GetConRateFromInr("YEN")
+
+	assert.NotNil(t, err)
+	assert.EqualError(t, err, "currency not supported")
+	assert.Equal(t, want, got)
+}
+
+// Money stored in INR regardless of Input Currency.
+// AddMoney
+func TestMoneyAddedInUsd(t *testing.T) {
+	moneyObject, _ := NewMoneyObject(100, "INR")
+	Wallet := NewWalletBalanceInInr(moneyObject)
+	MoneytoAdd := 200.0
+	Currency := "USD"
+	moneyObject2, _ := NewMoneyObject(MoneytoAdd, Currency)
+	want := 16594.0
+
+	err := Wallet.AddMoney(*moneyObject2)
+	got := Wallet.GetMoneyObjectInInr()
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, got.amount)
+}
+
+func TestMoneyAddedInInr(t *testing.T) {
+	moneyObject, _ := NewMoneyObject(100, "INR")
+	Wallet := NewWalletBalanceInInr(moneyObject)
+	MoneytoAdd := 200.0
+	Currency := "INR"
+	moneyObject2, _ := NewMoneyObject(MoneytoAdd, Currency)
+	want := 300.0
+
+	err := Wallet.AddMoney(*moneyObject2)
+	got := Wallet.GetMoneyObjectInInr()
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, got.amount)
+}
+
+//Rounding
+
+// debit money
+func TestMoneydebitedInUsd(t *testing.T) {
+	moneyObject, _ := NewMoneyObject(10000, "INR")
+	Wallet := NewWalletBalanceInInr(moneyObject)
+	MoneytoDebit := 20.5
+	Currency := "USD"
+	moneyObject2, _ := NewMoneyObject(MoneytoDebit, Currency)
+	want := 8309.37
+
+	err := Wallet.DebitMoney(*moneyObject2)
+	got := Wallet.GetMoneyObjectInInr()
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, got.amount)
+}
+
+func TestMoneydebitedInInr(t *testing.T) {
+	moneyObject, _ := NewMoneyObject(1000, "INR")
+	Wallet := NewWalletBalanceInInr(moneyObject)
+	MoneytoDebit := 200.0
+	Currency := "INR"
+	moneyObject2, _ := NewMoneyObject(MoneytoDebit, Currency)
+	want := 800.0
+
+	err := Wallet.DebitMoney(*moneyObject2)
+	got := Wallet.GetMoneyObjectInInr()
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, got.amount)
+}
+
+func TestMoneydebitedgreaterThanBalance(t *testing.T) {
+	moneyObject, _ := NewMoneyObject(1000, "INR")
+	Wallet := NewWalletBalanceInInr(moneyObject)
+	MoneytoDebit := 2000.0
+	Currency := "INR"
+	moneyObject2, _ := NewMoneyObject(MoneytoDebit, Currency)
+	want := 1000.0
+
+	err := Wallet.DebitMoney(*moneyObject2)
+	got := Wallet.GetMoneyObjectInInr()
+
+	assert.NotNil(t, err)
+	assert.EqualError(t, err, "insufficient funds")
+	assert.Equal(t, want, got.amount)
+}
+
+// Add currency
+func TestGetConRateForSGD(t *testing.T) {
+	want := 61.87
+	//AddCurrencyAndConvRateToInr("SGD", 61.87)
+	got, err := GetConRateFromInr("SGD")
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, got)
+}
+
+func TestConSgdtoInr(t *testing.T) {
+	inputSgd := 100.0
+	currency := "SGD"
+	//AddCurrencyAndConvRateToInr("SGD", 50)
+	moneyobject, _ := NewMoneyObject(inputSgd, currency)
+	want := 6187.0
+
+	got, err := ConverterToInr(*moneyobject)
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, got.amount)
+}
+func TestMoneyAddedInSgd(t *testing.T) {
+	moneyObject, _ := NewMoneyObject(100, "INR")
+	Wallet := NewWalletBalanceInInr(moneyObject)
+	//AddCurrencyAndConvRateToInr("SGD", 61.87)
+	MoneytoAdd := 200.0
+	Currency := "SGD"
+	moneyObject2, _ := NewMoneyObject(MoneytoAdd, Currency)
+	want := 12474.0
+
+	err := Wallet.AddMoney(*moneyObject2)
+	got := Wallet.GetMoneyObjectInInr()
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, got.amount)
+}
+
+func TestMoneydebitedInSgd(t *testing.T) {
+	moneyObject, _ := NewMoneyObject(10000, "INR")
+	Wallet := NewWalletBalanceInInr(moneyObject)
+	//AddCurrencyAndConvRateToInr("SGD", 61.87)
+	MoneytoDebit := 20.5
+	Currency := "SGD"
+	moneyObject2, _ := NewMoneyObject(MoneytoDebit, Currency)
+	want := 8731.67
+
+	err := Wallet.DebitMoney(*moneyObject2)
+	got := Wallet.GetMoneyObjectInInr()
+
+	assert.Nil(t, err)
+	assert.Equal(t, want, got.amount)
 }
